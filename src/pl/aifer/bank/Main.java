dd69c42714022c1bd766c4b7185ac0a842cb8e18
Aifer Altmyshbaeva <aifer.altmyshbaeva@gmail.com>
2023-05-15 00:37:34 +0200

    done

--- src/pl/aifer/bank/Main.java
+++ src/pl/aifer/bank/Main.java
@@ -1,28 +1,43 @@
 package pl.aifer.bank;
 
-import pl.aifer.bank.BankAccount;
-import pl.aifer.bank.LimitException;
-
 public class Main {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    public static final int INITIAL_AMOUNT = 20_000;
+    public static final int SUM = 6_000;
+
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
-        bankAccount.deposit(20_000);
+        bankAccount.deposit(INITIAL_AMOUNT);
         while (true){
             try {
-                bankAccount.withdraw(6_000);
+                withdraw(bankAccount, SUM);
             } catch (LimitException e) {
-                withdraw(bankAccount, (int) e.getRemainingAmount());
+                System.out.println(ANSI_RED + e.getMessage() + ANSI_RESET);
+                failedWithdraw(bankAccount, (int) e.getRemainingAmount());
                 break;
             }
         }
-        System.out.println("bankAccount = " + bankAccount);
+        System.out.println("Остаток на счете = " + bankAccount.getAmount());
     }
 
-    private static void withdraw(BankAccount bankAccount, int sum) {
-        try {
-            bankAccount.withdraw(sum);
-        } catch (LimitException e) {
+    private static void withdraw(BankAccount bankAccount, int sum) throws LimitException {
+        System.out.println(String.format("Остаток на счете = %.2f; сумма к снятию %d",
+                bankAccount.getAmount(),
+                sum));
+        bankAccount.withdraw(sum);
+    }
 
+    private static void failedWithdraw(BankAccount bankAccount, int sum) {
+        Exception throwable = null;
+        try {
+            withdraw(bankAccount, sum);
+        } catch (LimitException e) {
+            throwable = e;
+        } finally {
+            if (throwable != null) {
+                throw new RuntimeException("Произошла непредвиденная ошибка", throwable);
+            }
         }
     }
 }
